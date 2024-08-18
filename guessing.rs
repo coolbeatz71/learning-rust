067420a0818d2b269150538c38ae02bedbdc296e
Mutombo Jean-vincent <sigmacool@gmail.com>
2024-08-18 17:47:40 +0200

    [chore] create a loop that exit if guess is correct, colorize input/output values

--- guessing.rs
+++ guessing.rs
@@ -1,25 +1,38 @@
 use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
+use colored::Colorize;
+
 
 pub(crate) fn get_guessing() {
-  println!("Guess the number");
-  println!("Please input your guess.");  
+  println!("GUESS THE NUMBER GAME");
+  println!("=====================");
   
-  let mut guess: String = String::new();  
   let secret_number: u32 = rand::thread_rng().gen_range(1..101) as u32;
+  let mut guess: String = String::new();  
   println!("Guessing number: {}", secret_number);  
   
-  io::stdin().read_line(&mut guess).expect("Failed to read line");
-  
-  let guess: u32 = guess.trim().parse().expect("Please type a number!");
-  
-  println!("You guessed: {}", guess);
-
-  match guess.cmp(&secret_number) {
-    Ordering::Less => println!("Number too small!"),
-    Ordering::Greater => println!("Number too big!"),
-    Ordering::Equal => println!("Bingo! You win!"),
+  loop {
+    println!("{}", "Please input your guess.".blue().italic());  
+    
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    
+    let guess: u32 = match guess.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("Input must be a number!");
+        continue;
+      },
+    };
+    
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("{}", "Number too small!".red()),
+      Ordering::Greater => println!("{}", "Number too big!".red()),
+      Ordering::Equal => {
+        println!("{}", "Bingo! You win!".green().bold());
+        break;
+      },
+    }
   }
 }
 
