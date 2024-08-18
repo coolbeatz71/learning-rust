9e14f984f18d0fbe069424401013eaf4f5d0eef6
Mutombo Jean-vincent <sigmacool@gmail.com>
2024-08-18 17:00:06 +0200

    [chore] install rand package and use thread_rng to generate a random number

--- guessing.rs
+++ guessing.rs
@@ -1,10 +1,12 @@
 use std::io;
+use rand::Rng;
 
 pub(crate) fn get_guessing() {
   println!("Guess the number");
   println!("Please input your guess.");
 
-  let mut guess = String::new();
+  let mut guess = String::new();  
+  let secret_number = rand::thread_rng().gen_range(1..101);
 
   io::stdin().read_line(&mut guess).expect("Failed to read line");
 
